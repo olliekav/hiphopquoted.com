842a93eb76a3131ca324a619acb24f6019db455d
Ollie Kavanagh <olliek@gmail.com>
2014-07-10 22:26:58 -0400

    Big rebuild edits

---
+++ source/javascripts/application.js
@@ -0,0 +1,55 @@
+$(function() {
+  
+  // Hide Header on on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var footerHeight = $('#footer').outerHeight();
+
+  $(window).scroll(function(event){
+    didScroll = true;
+  });
+
+  setInterval(function() {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 250);
+
+  function hasScrolled() {
+    var st = $(this).scrollTop();
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    if (st > lastScrollTop && st > footerHeight -40){
+      $('#footer').removeClass('footer-up').addClass('footer-down');
+    } else {
+      if(st + $(window).height() < $(document).height()) {
+          $('#footer').removeClass('footer-down').addClass('footer-up');
+      }
+    }
+    lastScrollTop = st;
+  }
+
+  // Show navigation on click
+  $('.toggle-nav').on('click', function(e){
+    e.preventDefault();
+    if(!$(this).hasClass('active')) {
+      $(this).addClass('active');
+      $('body').addClass('nav-on');
+      $('.overlay-nav').addClass('on');
+    } else {
+      $(this).removeClass('active');
+      $('body').removeClass('nav-on');
+      $('.overlay-nav').removeClass('on');
+    }
+  });
+
+  $('.close-nav').on('click', function(e){
+    e.preventDefault();
+    $('.toggle-nav').removeClass('active');
+    $('body').removeClass('nav-on');
+    $('.overlay-nav').removeClass('on');
+  });
+
+});
