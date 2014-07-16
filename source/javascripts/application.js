06dedfbae8e196802d553147f472355d3233785e
Ollie Kavanagh <olliek@gmail.com>
2014-07-15 22:32:13 -0400

    Rebuilding again

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,34 +1,36 @@
 $(function() {
   
-  // Hide Header on on scroll down
-  var didScroll;
-  var lastScrollTop = 0;
-  var delta = 5;
-  var footerHeight = $('#footer').outerHeight();
+  if(!$('body').hasClass('page-index')) {
+    // Hide Header on on scroll down
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var footerHeight = $('#footer').outerHeight();
 
-  $(window).scroll(function(event){
-    didScroll = true;
-  });
+    $(window).scroll(function(event){
+      didScroll = true;
+    });
 
-  setInterval(function() {
-    if (didScroll) {
-      hasScrolled();
-      didScroll = false;
-    }
-  }, 250);
-
-  function hasScrolled() {
-    var st = $(this).scrollTop();
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    if (st > lastScrollTop && st > footerHeight -40){
-      $('#footer').removeClass('footer-up').addClass('footer-down');
-    } else {
-      if(st + $(window).height() < $(document).height()) {
-          $('#footer').removeClass('footer-down').addClass('footer-up');
+    setInterval(function() {
+      if (didScroll) {
+        hasScrolled();
+        didScroll = false;
       }
+    }, 250);
+
+    function hasScrolled() {
+      var st = $(this).scrollTop();
+      if(Math.abs(lastScrollTop - st) <= delta)
+          return;
+      if (st > lastScrollTop && st > footerHeight -40){
+        $('#footer').removeClass('footer-up').addClass('footer-down');
+      } else {
+        if(st + $(window).height() < $(document).height()) {
+            $('#footer').removeClass('footer-down').addClass('footer-up');
+        }
+      }
+      lastScrollTop = st;
     }
-    lastScrollTop = st;
   }
 
   // Show navigation on click
