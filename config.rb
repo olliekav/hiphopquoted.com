efecd7381f708c55ee3ad5069868dd2827f38af8
Ollie Kavanagh <olliek@gmail.com>
2016-02-14 23:29:02 -0500

    Update config.rb

--- config.rb
+++ config.rb
@@ -50,12 +50,6 @@ end
 
 activate :directory_indexes
 
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
-
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
